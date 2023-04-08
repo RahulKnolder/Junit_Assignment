4c9cea3f092de37d7181e46c64124a9c53adb1f5
ChilledCoder <rahul.sinha@knoldus.com>
2023-04-08 17:19:52 +0530

    refactoring my assignment

--- src/main/java/juint_5/Main.java
+++ src/main/java/juint_5/Main.java
@@ -14,10 +14,52 @@ public class Main {
         scanner.nextLine();
 
 
-        System.out.println("enter name");
+        System.out.println("Enter Name");
         String name = scanner.nextLine();
 
-        System.out.println("enter email in correct formate");
+        System.out.println("enter e-mail in correct formate");
+        String email = scanner.nextLine();
+
+        //loading constructor with values
+
+
+        person.setEmail(email);
+
+        //custom exception
+        try {
+            person.setAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        person.setName(name);
+
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
+        System.out.println(person.getEmail());
+    }
+}
+package juint_5;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter age ");
+        Integer age = scanner.nextInt();
+        scanner.nextLine();
+
+
+        System.out.println("Enter Name");
+        String name = scanner.nextLine();
+
+        System.out.println("enter e-mail in correct formate");
         String email = scanner.nextLine();
 
         //loading constructor with values
