d5e2f41d997b6198c5e7fe546d9a67633ee519c4
ChilledCoder <rahul.sinha@knoldus.com>
2023-04-11 17:21:14 +0530

    worked on my pr reviews

--- src/test/java/juint_5/PersonTest.java
+++ src/test/java/juint_5/PersonTest.java
@@ -9,60 +9,81 @@ public class PersonTest {
     Person person;
 
     @BeforeEach
-    public void createInstance() {
+    public void setup() {
         person = new Person();
     }
 
     @Test
-    public void testGetName() {
+    public void testGetName_NotNullValues() {
         //test getname with not null values
         person.setName("rahul");
         assertNotNull(person.getName());
+    }
 
+    @Test
+    public void testGetName_nullValue() {
         // Test the getter method for a null value
         person.setName(null);
         assertEquals("", person.getName());
 
+    }
+
+    @Test
+    public void testGetName_WhiteSpaces() {
         //testing getname with white spaces
         person.setName("         ");
         assertEquals("", person.getName());
     }
 
-
     @Test
-    public void testGetAge() throws InvalidAgeException {
+    public void testGetAge_nonNullValue() throws InvalidAgeException {
         // Test the getter method for a non-null value
         person.setAge(56);
         assertNotNull(person.getAge());
+    }
 
+    @Test
+    public void testGetAge_nullValue() throws InvalidAgeException {
         // Test the getter method for a null value
         InvalidAgeException e = assertThrows(InvalidAgeException.class, () -> {
             person.setAge(null);
         });
-        assertNotNull(person.getAge());
+        assertNull(person.getAge());
     }
 
 
     @Test
-    public void testGetEmail() {
+    public void testGetEmail_nonNullValue() {
         // Test the getEmail() method for a non-null value
         person.setEmail("john.doe@example.com");
         assertNotNull(person.getEmail());
+    }
 
+    @Test
+    public void testGetEmail_nullValue() {
         // Test the getEmail() method for a null value
         person.setEmail(null);
         String email = person.getEmail();
         assertEquals("", email);
 
+    }
 
+    @Test
+    public void testGetEmail_validValue() {
         //test for a valid value
         person.setEmail("john.doe@example.com");
         assertEquals("john.doe@example.com", person.getEmail());
+    }
 
+    @Test
+    public void testGetEmail_invalidInput() {
         //testing with invalidInput
         person.setEmail("rahul@@gmai.com");
-        assertEquals("",person.getEmail());
+        assertEquals("", person.getEmail());
+    }
 
+    @Test
+    public void testGetEmail_whiteSpaces() {
         //for white spaces
         person.setEmail("   ");
         assertEquals("", person.getEmail());
